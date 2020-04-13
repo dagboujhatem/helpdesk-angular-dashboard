9852c6f4ba4140ecf4107ae9721d879d97561080
Dagouj-Hatem <dagboujhatem@gmail.com>
2020-04-13 14:50:30 +0200

    Gestion des mission & refactoring added by oumaima

--- src/app/views/categorie/categorie-add/categorie-add.component.ts
+++ src/app/views/categorie/categorie-add/categorie-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-categorie-add',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategorieAddComponent implements OnInit {
 
-  constructor() { }
+  categorieAddForum: FormGroup;
+  submitted = false;
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) { }
+
+
+  ngOnInit() {
+    this.categorieAddForum = this.formBuilder.group({
+        element: ['', [Validators.required]],
+        type: ['', [Validators.required]],
+         description: ['', [Validators.required]],
+        solution: ['', [Validators.required]],
+    });
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.categorieAddForum.controls;}
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.
+      categorieAddForum.invalid) {
+        return;
+    }
+
+    // display form values on success
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this. categorieAddForum.value, null, 4));
   }
 
 }
