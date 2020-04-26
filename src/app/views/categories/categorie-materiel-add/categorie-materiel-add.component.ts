7f816f7b627459b2fab61a18ff765e0fec7a4d6c
Dagouj-Hatem <dagboujhatem@gmail.com>
2020-04-26 22:59:39 +0200

    les formulaires de gestion des catégories

--- src/app/views/categories/categorie-materiel-add/categorie-materiel-add.component.ts
+++ src/app/views/categories/categorie-materiel-add/categorie-materiel-add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-categorie-materiel-add',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categorie-materiel-add.component.css']
 })
 export class CategorieMaterielAddComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+  CategorieMaterielAddForm: FormGroup;
+    submitted = false;
+    constructor(private formBuilder: FormBuilder
+  
+    ) { }
+  
+    ngOnInit() {this. CategorieMaterielAddForm = this.formBuilder.group({
+      description: ['', [Validators.required],
+      ]
+  });
+    }
+    onSubmit() {
+      this.submitted = true;
+  
+      // stop here if form is invalid
+      if (this. CategorieMaterielAddForm.invalid) {
+          return;
+      }}}
+  
--- src/app/views/categories/categorie-materiel.service.ts
+++ src/app/views/categories/categorie-materiel.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategorieMaterielService {
 
-  constructor() { }
+  url = environment.serverUrl;
+
+  constructor(private http: HttpClient) { }
+
+  // Get all  categories materiels from the REST API
+  getCategorieMateriel() {
+    return this.http.get(this.url + 'categories');
+  }
+
+  // Get categorie materiel by ID from the REST API
+  getCategorieMaterielById (id) {
+    return this.http.get(this.url + 'categories/' + id);
+  }
+
+  // Add a categorie materiel from the REST API
+  addCategorieMateriel(CategorieMaterielData) {
+   return  this.http.post(this.url + 'categories', CategorieMaterielData);
+  }
+
+  // Update a categorie materiel from the REST API
+  updateCategorieMateriel(CategorieMaterielID , CategorieMaterielNewData) {
+    return this.http.put(this.url + 'categories/' + CategorieMaterielID, CategorieMaterielNewData);
+  }
+
+  // Delete a categorie materiel from the REST API
+  deleteCategorieMateriel(CategorieMaterielID) {
+    return this.http.delete(this.url + 'categories/' + CategorieMaterielID);
+  }
 }
+
