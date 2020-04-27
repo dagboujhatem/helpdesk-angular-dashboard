a1d0f0dca153fc580c7a16b2d46555a219d52cab
Dagbouj-Hatem <dagboujhatem@gmail.com>
2020-04-27 01:12:03 +0100

    add datatable service

--- src/app/views/acces/acces-index/acces-index.component.ts
+++ src/app/views/acces/acces-index/acces-index.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import {AccesService} from '../acces.service';
 import {ToasterService} from 'angular2-toaster';
+import {DataTableService} from '../../common/utils/data-table.service';
 
 @Component({
   selector: 'app-acces-index',
@@ -9,54 +10,16 @@ import {ToasterService} from 'angular2-toaster';
 })
 export class AccesIndexComponent implements OnInit {
 
-  public usersData: any = null ;
+  public usersData: any = null;
   dtOptions: DataTables.Settings = {};
 
   constructor(private accesService: AccesService,
-              private toasterService: ToasterService) { }
+              private toasterService: ToasterService,
+              private dataTableService: DataTableService) { }
 
 
   ngOnInit() {
-    // @ts-ignore
-    // @ts-ignore
-    this.dtOptions = {
-      pagingType: 'full_numbers',
-      pageLength: 5,
-      processing: true,
-      data: this.usersData,
-      // Declare the use of the extension in the dom parameter
-      dom: 'Bfrtip',
-      // Configure the buttons
-      // buttons: [
-      //   'print',
-      //   'excel',
-      // ],
-      // Use this attribute to enable the responsive extension
-      responsive: false,
-      /* below is the relevant part, e.g. translated to spanish */
-      language: {
-        processing: 'Traitement...',
-        search: 'Rechercher :',
-        lengthMenu: 'Afficher _MENU_ éléments',
-        info: 'Affichage de l\'élément _START_ à _END_ sur _TOTAL_ éléments',
-        infoEmpty: 'Affichage de l\'élément 0 à 0 sur 0 élément',
-        infoFiltered: '(filtré à partir de _MAX_ éléments au total',
-        infoPostFix: '',
-        loadingRecords: 'Chargement...',
-        zeroRecords: 'Aucun élément correspondant trouvé',
-        emptyTable: 'Aucune donnée disponible dans le tableau',
-        paginate: {
-          first: 'Premier',
-          previous: 'Dernier',
-          next: 'Suivant',
-          last: 'Précédent'
-        },
-        aria: {
-          sortAscending: ': activer pour trier la colonne par ordre croissant',
-          sortDescending: ': activer pour trier la colonne par ordre décroissant'
-        }
-      }
-    };
+    // this.dtOptions = this.dataTableService.getDataTableOptions();
     this.getAllUsers();
   }
 
