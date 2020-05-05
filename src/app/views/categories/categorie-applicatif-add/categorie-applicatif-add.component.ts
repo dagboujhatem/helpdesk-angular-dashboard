670376c5c2d6ea86468166fea50ea3ed4cc6b47f
Dagbouj-Hatem <dagboujhatem@gmail.com>
2020-05-05 23:47:18 +0100

    catégorie applicatif add refactoring

--- src/app/views/categories/categorie-applicatif-add/categorie-applicatif-add.component.ts
+++ src/app/views/categories/categorie-applicatif-add/categorie-applicatif-add.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {CategorieApplicatifService} from '../categorie-applicatif.service';
+import {ToasterService} from 'angular2-toaster';
+import {ValidationService} from '../../common/utils/validation.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-categorie-applicatif-add',
@@ -7,10 +11,16 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./categorie-applicatif-add.component.css']
 })
 export class CategorieApplicatifAddComponent implements OnInit {
+
+  selectedFile = null;
   categorieApplicatifAddForm: FormGroup;
   submitted = false;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+              private categorieApplicatifService: CategorieApplicatifService,
+              private toasterService: ToasterService,
+              private validationService: ValidationService,
+              private router: Router) { }
 
   ngOnInit() {
     this.categorieApplicatifAddForm = this.formBuilder.group({
@@ -19,6 +29,13 @@ export class CategorieApplicatifAddComponent implements OnInit {
       description: ['', Validators.required],
       solutionFile: ['', Validators.required]
       });
+  }
+
+  changeSelectedFile(files) {
+    if (files.length === 0) {
+      return;
+    }
+    this.selectedFile = files[0];
   }
 
   // convenience getter for easy access to form fields
@@ -32,5 +49,22 @@ export class CategorieApplicatifAddComponent implements OnInit {
     if (this.categorieApplicatifAddForm.invalid) {
         return;
     }
+
+    // Create a request body data
+    const requestBody = new FormData();
+    requestBody.append('type', this.categorieApplicatifAddForm.get('type').value);
+    requestBody.append('probleme', this.categorieApplicatifAddForm.get('probleme').value);
+    requestBody.append('description', this.categorieApplicatifAddForm.get('description').value);
+    requestBody.append('solution_file', this.selectedFile, this.selectedFile.name);
+
+    this.categorieApplicatifService.addCategorieApplicatif(requestBody).subscribe(
+      (bodyResponse) => { this.processResponse(bodyResponse); },
+      (error) => { this.validationService.showValidationsMessagesInToast(error);}
+    );
+  }
+
+  processResponse(bodyResponse) {
+    this.toasterService.pop('success', 'Catégorie applicatif ajoutée:', bodyResponse.message);
+    this.router.navigate(['/home/categories/applicatif/index']);
   }
 }
