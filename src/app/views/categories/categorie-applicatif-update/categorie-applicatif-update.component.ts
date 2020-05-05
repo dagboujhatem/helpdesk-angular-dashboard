c443a0f52ab3361c2902512c2e7e380fa08b9191
Dagbouj-Hatem <dagboujhatem@gmail.com>
2020-05-05 23:19:10 +0100

    catégorie applicatif update refactoring

--- src/app/views/categories/categorie-applicatif-update/categorie-applicatif-update.component.ts
+++ src/app/views/categories/categorie-applicatif-update/categorie-applicatif-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute} from '@angular/router';
+import {CategorieApplicatifService} from '../categorie-applicatif.service';
 
 @Component({
   selector: 'app-categorie-applicatif-update',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categorie-applicatif-update.component.css']
 })
 export class CategorieApplicatifUpdateComponent implements OnInit {
+  categirieApplicatifID = null ;
+  categorieApplicatifUpdateForm: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+              private route: ActivatedRoute,
+              private categorieApplicatifService: CategorieApplicatifService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.categirieApplicatifID = this.route.snapshot.paramMap.get('id');
+    this.categorieApplicatifService.getCategorieApplicatifById(this.categirieApplicatifID).subscribe(
+      (bodyResponse) => { this.loadCategorieData(bodyResponse); }
+    );
+    this.categorieApplicatifUpdateForm = this.formBuilder.group({
+      type: ['', Validators.required],
+      probleme: ['', Validators.required],
+      description: ['', Validators.required],
+      solutionFile: ['', ]
+    });
   }
 
+  loadCategorieData(bodyResponse) {
+    const data = bodyResponse.data;
+    this.categorieApplicatifUpdateForm.patchValue(data);
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.categorieApplicatifUpdateForm.controls; }
+
+  // on submit
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.categorieApplicatifUpdateForm.invalid) {
+      return;
+    }
+  }
 }
