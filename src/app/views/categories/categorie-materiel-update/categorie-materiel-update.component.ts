cfae0262077307a1399812598dad291e14327d2b
Dagouj-Hatem <dagboujhatem@gmail.com>
2020-05-06 21:56:18 +0200

    materiel

--- src/app/views/categories/categorie-materiel-update/categorie-materiel-update.component.ts
+++ src/app/views/categories/categorie-materiel-update/categorie-materiel-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { CategorieMaterielService } from '../categorie-materiel.service';
 
 @Component({
   selector: 'app-categorie-materiel-update',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategorieMaterielUpdateComponent implements OnInit {
 
-  constructor() { }
+  categirieMaterielID = null ;
+  categorieMaterielUpdateForm: FormGroup;
+  submitted = false;
+  
+  constructor(private formBuilder: FormBuilder,
+              private route: ActivatedRoute,
+              private categorieMaterielService: CategorieMaterielService) { }
+   ngOnInit() {
+  this.categirieMaterielID = this.route.snapshot.paramMap.get('id');
+  this.categorieMaterielService.getCategorieMaterielById(this.categirieMaterielID).subscribe(
+    (bodyResponse) => { this.loadCategorieData(bodyResponse); }
+  );
+  this.categorieMaterielUpdateForm = this.formBuilder.group({
+    type: ['', Validators.required],
+    probleme: ['', Validators.required],
+    description: ['', Validators.required],
+    solutionFile: ['', ]
+  });
+}
 
-  ngOnInit(): void {
+loadCategorieData(bodyResponse) {
+  const data = bodyResponse.data;
+  this.categorieMaterielUpdateForm.patchValue(data);
+}
+
+// convenience getter for easy access to form fields
+get f() { return this.categorieMaterielUpdateForm.controls; }
+
+// on submit
+onSubmit() {
+  this.submitted = true;
+
+  // stop here if form is invalid
+  if (this.categorieMaterielUpdateForm.invalid) {
+    return;
   }
+}
 
 }
--- src/app/views/categories/categorie-materiel.service.ts
+++ src/app/views/categories/categorie-materiel.service.ts
@@ -13,27 +13,27 @@ export class CategorieMaterielService {
 
   // Get all  categories materiels from the REST API
   getCategorieMateriel() {
-    return this.http.get(this.url + 'categories');
+    return this.http.get(this.url + 'categorieMateriels');
   }
 
   // Get categorie materiel by ID from the REST API
   getCategorieMaterielById (id) {
-    return this.http.get(this.url + 'categories/' + id);
+    return this.http.get(this.url + 'categorieMateriels/' + id);
   }
 
   // Add a categorie materiel from the REST API
   addCategorieMateriel(CategorieMaterielData) {
-   return  this.http.post(this.url + 'categories', CategorieMaterielData);
+   return  this.http.post(this.url + 'categorieMateriels', CategorieMaterielData);
   }
 
   // Update a categorie materiel from the REST API
   updateCategorieMateriel(CategorieMaterielID , CategorieMaterielNewData) {
-    return this.http.put(this.url + 'categories/' + CategorieMaterielID, CategorieMaterielNewData);
+    return this.http.put(this.url + 'categorieMateriels/' + CategorieMaterielID, CategorieMaterielNewData);
   }
 
   // Delete a categorie materiel from the REST API
   deleteCategorieMateriel(CategorieMaterielID) {
-    return this.http.delete(this.url + 'categories/' + CategorieMaterielID);
+    return this.http.delete(this.url + 'categorieMateriels/' + CategorieMaterielID);
   }
 }
 
